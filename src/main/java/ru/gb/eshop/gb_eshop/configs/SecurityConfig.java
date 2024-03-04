acda6e6e7ab1f5748f328b8e487549388eafc602
iopa <pakulin.yury@gmail.com>
2024-03-04 20:42:39 +0300

    services + security

---
+++ src/main/java/ru/gb/eshop/gb_eshop/configs/SecurityConfig.java
@@ -0,0 +1,57 @@
+package ru.gb.eshop.gb_eshop.configs;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.gb.eshop.gb_eshop.services.PersonDetailsService;
+
+@Configuration
+public class SecurityConfig {
+    private final PersonDetailsService personDetailsService;
+
+    @Bean
+    public PasswordEncoder getPasswordEncode() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/admin").hasAnyAuthority("ROLE_ADMIN")
+                .requestMatchers("/authentication", "/registration", "/error", "/resources/**", "/static/**", "/css/**", "/js/**",
+                        "/pics/**", "/images/**", "/product", "/product/info/{id}", "/product/search", "/product/searchHeader", "/logout").permitAll()
+                //.anyRequest().hasAnyAuthority("USER", "ADMIN")
+                .anyRequest()
+                .authenticated()
+        );
+        http.formLogin(login -> login
+                .loginPage("/authentication")
+                //.usernameParameter("name")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/personalAccount",true)
+                .failureUrl("/authentication?error")
+                .permitAll());
+        http.logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/authentication")
+                .permitAll());
+        // http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(personDetailsService)
+                .passwordEncoder(getPasswordEncode());
+    }
+}
---
+++ src/main/java/ru/gb/eshop/gb_eshop/services/PersonService.java
@@ -0,0 +1,69 @@
+package ru.gb.eshop.gb_eshop.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.gb.eshop.gb_eshop.enums.Role;
+import ru.gb.eshop.gb_eshop.models.Person;
+import ru.gb.eshop.gb_eshop.repositories.PersonRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PersonService {
+    private final PersonRepository personRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository, PasswordEncoder passwordEncoder) {
+        this.personRepository = personRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Person findByLogin(Person person) {
+        Optional<Person> person_db = personRepository.findByLogin(person.getLogin());
+        return person_db.orElse(null);
+    }
+
+    @Transactional
+    public void register(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
+        person.setRole(Role.ROLE_USER);
+        personRepository.save(person);
+    }
+
+
+    public List<Person> getAllPerson() {
+        return personRepository.findAll();
+    }
+
+
+    public Person getPersonById(int id) {
+        Optional<Person> optionalPerson = personRepository.findById(id);
+        return optionalPerson.orElse(null);
+    }
+
+    @Transactional
+    public void updatePerson(int id, Person person) {
+        person.setId(id);
+        personRepository.save(person);
+    }
+
+    @Transactional
+    public void deletePerson(int id) {
+        personRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void changePassword(int id, String password) {
+        personRepository.updatePersonById(id, passwordEncoder.encode(password));
+    }
+
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+
+}
