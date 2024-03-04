aff5089d63a093783fbf17e5b7314fc61103201c
iopa <pakulin.yury@gmail.com>
2024-03-04 20:23:57 +0300

    init

---
+++ src/main/java/ru/gb/eshop/gb_eshop/GbEshopApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.eshop.gb_eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbEshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbEshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/eshop/gb_eshop/GbEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.eshop.gb_eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbEshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
