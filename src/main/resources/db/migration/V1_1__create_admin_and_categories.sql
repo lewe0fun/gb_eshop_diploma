8cfdc4f5cd5b7680e6fb734f93938284ccfd0902
iopa <pakulin.yury@gmail.com>
2024-03-07 21:02:14 +0300

    flyway fix

--- src/main/resources/db/migration/V1_1__create_admin_and_categories.sql
+++ src/main/resources/db/migration/V1_1__create_admin_and_categories.sql
@@ -2,4 +2,4 @@ INSERT INTO Person (login, password, role)
 VALUES ('admin', '$2a$10$IShKtJaTQ0mqx.YzDqIv6eWVOBgZ3Gspc7/tEcjl7PZd8aKNJ4V8C', 'ROLE_ADMIN');
 
 INSERT INTO category("name")
-VALUES ('furniture'),('appliances'),('clothes'),('shower stalls'),('kitchen sinks'),('fittings');
+VALUES ('baths'),('sanitary ware'),('mixers'),('shower stalls'),('kitchen sinks'),('fittings');
